ad4cdba498b35581f7dbd19bf940abae387012e1
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-10-21 08:13:27 -0300

    feat: add Categories and CategoryItem component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import { Categories } from './components/categories'
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner_01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês!"
+      />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>a</h1>
-    </div>
-  )
-}
