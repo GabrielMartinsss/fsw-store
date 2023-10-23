ef59452d91bb4c597e150878fdba5ab3d23b78a0
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-10-23 09:16:22 -0300

    feat: add Footer component to layout

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="mt-8 bg-accent px-8 py-4 text-center text-[0.625rem] text-zinc-500">
+      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+    </div>
+  )
+}
