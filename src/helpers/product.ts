6137cfd8c2e70bedd198925158976b0a053a4a8c
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-10-23 07:04:13 -0300

    feat: add computeProductTotalPrice helper function

---
+++ src/helpers/product.ts
@@ -0,0 +1,25 @@
+import { Product } from '@prisma/client'
+
+interface ProductWithTotalPrice extends Product {
+  totalPrice: number
+}
+
+export function computeProductTotalPrice(
+  product: Product,
+): ProductWithTotalPrice {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    }
+  }
+
+  const discount =
+    Number(product.basePrice) * (product.discountPercentage / 100)
+  const totalPrice = Number(product.basePrice) - discount
+
+  return {
+    ...product,
+    totalPrice,
+  }
+}
