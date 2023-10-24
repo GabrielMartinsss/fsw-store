10d7c2aa15f2e6a5d5ac608e1d51478bb5d3cc5b
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-10-24 07:49:13 -0300

    feat: ensure user goes to category page on category item click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,18 +2,21 @@ import { ShoppingCartIcon } from 'lucide-react'
 import { Button } from './button'
 import { Card } from './card'
 import SideMenu from '../sideMenu'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <SideMenu />
 
-      <h1 className="text-lg font-bold">
-        <span className="bg-gradient-to-r from-[#5033c3] to-[#8162ff] bg-clip-text text-transparent">
-          FSW
-        </span>
-        Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-bold">
+          <span className="bg-gradient-to-r from-[#5033c3] to-[#8162ff] bg-clip-text text-transparent">
+            FSW
+          </span>
+          Store
+        </h1>
+      </Link>
 
       <Button variant="outline" size="icon">
         <ShoppingCartIcon />
