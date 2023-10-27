f320faf945a15dd2e48916a29b1ee9f85ede27e2
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-10-27 09:32:27 -0300

    feat: ensure product on cart can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -6,13 +6,15 @@ import { useContext } from 'react'
 import { CartContext } from '@/providers/cart'
 import CartItem from './cart-item'
 import { Separator } from './separator'
+import { ScrollArea } from './scroll-area'
+import { Button } from './button'
 
 export default function Cart() {
   const { products, cartSubTotalPrice, cartTotalDiscount, cartTotalPrice } =
     useContext(CartContext)
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -21,15 +23,17 @@ export default function Cart() {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem key={product.name} product={product} />
-          ))
-        ) : (
-          <p className="font-semibold">Seu carrinho está vazio!</p>
-        )}
-      </div>
+      <ScrollArea className="flex-1">
+        <div className="flex flex-col gap-3">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <CartItem key={product.name} product={product} />
+            ))
+          ) : (
+            <p className="font-semibold">Seu carrinho está vazio!</p>
+          )}
+        </div>
+      </ScrollArea>
 
       {products.length > 0 && (
         <div className="flex flex-col gap-3">
@@ -53,6 +57,7 @@ export default function Cart() {
             <p>Total</p>
             <p>R$ {cartTotalPrice.toFixed(2)}</p>
           </div>
+          <Button className="mt-8 font-bold uppercase">Finalizar compra</Button>
         </div>
       )}
     </div>
