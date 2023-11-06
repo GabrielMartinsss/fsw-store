f8d5aa0b87e36abd5c01e7cf7de6db31f11040d7
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-11-06 06:33:05 -0300

    style: fiz border radius Header Component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -9,7 +9,7 @@ import LoginMenu from './login-menu'
 export default function Header() {
   return (
     <header className="relative z-10 my-16">
-      <Card className="fixed left-0 right-0 top-0 flex w-full items-center justify-between p-[1.875rem] md:px-24">
+      <Card className="fixed left-0 right-0 top-0 flex w-full items-center justify-between rounded-none p-[1.875rem] md:px-24">
         <SideMenu />
 
         <Link href="/">
