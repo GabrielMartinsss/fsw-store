60bb00c2af775320aa1e8cb4bf89f988960d31e5
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-11-10 18:37:51 -0300

    feat: add loading on all pages

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className="h-[120px] w-[120px] animate-spin rounded-full border-4 border-t-4 border-light-active-accent border-t-primary dark:border-accent dark:border-t-primary"></div>
+  )
+}
